d7d6d250ade67edd7cd87fc5bb3d33c05b2ecb15
Gabriel Matozinho <109115976+Gabriel-Matozinho@users.noreply.github.com>
2023-09-20 21:22:08 -0300

    Create Ex3.py

---
+++ Manipulando_Cadeia_Texto/Exercicios/Ex3.py
@@ -0,0 +1,5 @@
+Exercício Python 024: Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".
+
+cidade = str(input('Digite o  nome da cidade: ')).upper().split()
+resp = 'SANTOS'  in cidade
+print(resp)
