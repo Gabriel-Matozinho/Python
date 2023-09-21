0940f3c17bd6b672c5a33b7d03a4d845c6d85159
Gabriel Matozinho <109115976+Gabriel-Matozinho@users.noreply.github.com>
2023-09-20 21:11:30 -0300

    Create Ex1.py

---
+++ Manipulando_Cadeia_Texto/Exercicios/Ex1.py
@@ -0,0 +1,10 @@
+Exercício Python 01: Crie um programa que leia o nome completo de uma pessoa e mostre: 
+#- O nome com todas as letras maiúsculas e minúsculas.
+#- Quantas letras ao todo (sem considerar espaços).
+#- Quantas letras tem o primeiro nome.
+nome = str(input('Digite seu nome completo: ')).strip()
+n1 = nome.split()
+print(f'Seu nome em maiúsculo é {nome.upper()} ')
+print(f'Seu nome em minúsculo é {nome.lower()} ')
+print(f'Seu nome tem ao todo {len(nome)} ')
+print(f'Seu primeiro nome é {n1[0]} e ele tem {len(n1[0])}')
